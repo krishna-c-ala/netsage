94b65501717cc61aa502e95fe90b4360c4ba0f92
cehtraining12 <cehtraining12@gmail.com>
2026-07-16 14:31:12 +0100

    Add Initial project structure

---
+++ app.py
@@ -0,0 +1,10 @@
+def display_banner():
+    print('='*50)
+    print('Netsage v0.1')
+    print('Intelligent Configuration seearch')
+    print('='*50)
+def main():
+    display_banner()
+
+if __name__ == "__main__":
+    main()

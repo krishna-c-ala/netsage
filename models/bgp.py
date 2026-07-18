706ba5e33d580d1ab91a45503d046845b38de7f2
krishna <cehtraining12@gmail.com>
2026-07-18 17:42:49 +0100

    Create BGP neighbor model

---
+++ models/bgp.py
@@ -0,0 +1,10 @@
+"""
+The Class is the blank form. It just defines what pieces of information you need to collect 
+(e.g., Company Name, Role, Date Applied). The blank form itself doesn't contain anyone's data yet; it’s just the structure.
+
+The Object is a filled-out form. When you take that blank form and write "Google, Program Manager, July 18th" on it, you have created an actual, 
+specific instance of that form.
+"""
+class BGPNeighbor:
+    pass        #pass doesn nothing and just a placeholder.
+
